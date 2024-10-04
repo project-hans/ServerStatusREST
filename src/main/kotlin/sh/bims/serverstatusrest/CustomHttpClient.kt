a54b8a3a174ecf0e4ae77d2dfa933de05f639a43
caesarakalaeii <caesarlp@gmx.de>
2024-10-04 20:22:53 +0200

    init commit

---
+++ src/main/kotlin/sh/bims/serverstatusrest/CustomHttpClient.kt
@@ -0,0 +1,30 @@
+package sh.bims.serverstatusrest;
+
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.gson.*
+import kotlinx.coroutines.runBlocking
+
+data class ServerResponse(val servername: String)
+
+public class CustomHttpClient {
+    var OPERATOR_URL: String =System.getenv("OPERATOR_URL") ?: "main"
+    private val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            gson()
+        }
+    }
+
+    fun getServerName(): String? = runBlocking {
+        try {
+            val response: ServerResponse = client.get(OPERATOR_URL).body()
+            response.servername
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
---
+++ src/main/kotlin/sh/bims/serverstatusrest/ServerStatusREST.kt
@@ -0,0 +1,67 @@
+package sh.bims.serverstatusrest
+
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.http.*
+import io.ktor.serialization.gson.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.minecraft.server.MinecraftServer
+
+class ServerStatusREST : ModInitializer {
+    private var server: MinecraftServer? = null
+    var SERVER_NODE: String = System.getenv("SERVER_NODE") ?: "main"
+    var SERVER_NAME: String = CustomHttpClient().getServerName() ?: "server"
+
+
+
+
+    override fun onInitialize() {
+        // Register server lifecycle event
+        ServerLifecycleEvents.SERVER_STARTED.register(ServerLifecycleEvents.ServerStarted { minecraftServer ->
+            server = minecraftServer
+            startKtorServer()
+        })
+    }
+
+    private fun startKtorServer() {
+        embeddedServer(Netty, port = 25565) {
+            install(ContentNegotiation) {
+                gson()
+            }
+            routing {
+                get("/playercount") {
+                    val playerCount = server?.currentPlayerCount
+                    call.respond(mapOf("playerCount" to playerCount))
+                }
+                get("/maxplayercount") {
+                    val maxplayerCount = server?.maxPlayerCount
+                    call.respond(mapOf("maxplayerCount" to maxplayerCount))
+                }
+                get("/nodetype") {
+                    call.respond(mapOf("nodetype" to SERVER_NODE))
+                }
+                get("/status") {
+                    call.respond(HttpStatusCode.OK)
+                }
+                get("/servername") {
+                    call.respond(mapOf("server_name" to SERVER_NAME))
+                }
+                get("/coffee") {
+                    call.respond(HttpStatusCode.fromValue(418))
+                }
+            }
+        }.start(wait = false)
+
+    }
+
+
+
+}
